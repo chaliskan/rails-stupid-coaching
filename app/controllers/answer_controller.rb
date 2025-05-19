3db4403f93aa3342752960d36f006b415dfde6ac
Zeynep ÇaCaliskan <zeynepcaliskan@hacettepe.edu.tr>
2025-05-19 15:22:01 +0300

    stupidcoaching

---
+++ app/controllers/answer_controller.rb
@@ -0,0 +1,12 @@
+class AnswerController < ApplicationController
+  def answer
+    @question = params[:question]
+    @answer = if @question == "i am going to work"
+                "Great!"
+              elsif @question
+                "Silly question, get dressed and go to work!"
+              else
+                "I don't care, get dressed and go to work!"
+              end
+  end
+end
